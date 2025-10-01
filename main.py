244fd4bcfe1eab9022bb7c9f6e7c1a060fa772cd
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-01 15:03:18 +0300

    ДЗ:Создание простого калькуляторва. использование одного try

--- main.py
+++ main.py
@@ -3,39 +3,27 @@ try:
     num1 = float(input("Введите первое число: "))
     num2 = float(input("Введите второе число: "))
     result1 = num1 + num2
-except ValueError:
-    print("Ошибка: введено не число!")
-else:
     print(f"Результат сложения: {result1}")
 
-try:
     print("Вычитание")
     num3 = float(input("Введите первое число: "))
     num4 = float(input("Введите второе число: "))
     result2 = num3 - num4
-except ValueError:
-    print("Ошибка: введено не число!")
-else:
     print(f"Результат вычитания: {result2}")
 
-try:
     print("Умножение")
     num5 = float(input("Введите первое число: "))
     num6 = float(input("Введите второе число: "))
     result3 = num5 * num6
-except ValueError:
-    print("Ошибка: введено не число!")
-else:
     print(f"Результат умножения: {result3}")
 
-try:
     print("Деление")
     num7 = float(input("Введите первое число: "))
     num8 = float(input("Введите второе число: "))
     result = num7 / num8
+    print(f"Результат деления: {result}")
 except ValueError:
     print("Ошибка: введено не число!")
 except ZeroDivisionError:
     print("Ошибка: Деление на ноль!")
-else:
-    print(f"Результат деления: {result}")
+
