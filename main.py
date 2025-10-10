368a69fe75fe0ed51f9325d850fba56cfa12bec8
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-10 09:02:47 +0300

    ДЗ:Логические выражения и условия

--- main.py
+++ main.py
@@ -1,26 +1,13 @@
-def max_number(num1, num2):
-    if num1 > num2:
-        return num1
-    return num2
+def voit(age, citizenship, disqualification):
+    if (age < 18): print("Вам меньше 18 лет, голосовать нельзя")
+    elif (citizenship != "Russia"): print("Вы не гражданин, голосовать нельзя")
+    elif (disqualification == True): print("Вы отстранены от голосования")
+    else: print("Голосуйте")
 
-def empty_function():
-    pass
-
-def even_numbers(n):
-    for i in range(0, n):
-        if i % 2 == 0:
-            yield i
-
-def  test_max_number():
-    assert max_number(3, 5) == 5, "Ошибка, не равно 5"
-    assert max_number(4, 2) == 4, "Ошибка, не равно 4"
-    assert max_number(8, 8) == 8, "Ошибка, не равно 8"
-    print("Тесты пройдены")
-
-for i in even_numbers(5):
-    print(i)
-
-test_max_number()
+voit(18, "Russia", False)
+voit(20, "Russa", False)
+voit(16, "Russia", False)
+voit(20, "Russia", True)
 
 
 
