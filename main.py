19f1fc7e2c0590aea0c328dcf0efe0ae487ea6bf
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-14 17:02:01 +0300

    ДЗ:Анализ текста_корректировка

--- main.py
+++ main.py
@@ -1,35 +1,67 @@
-text = input("Введите текст: ").lower()
+def clean_text(text):
+    punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+    cleaned_text = ""
+    for char in text.lower():
+        if char not in punctuation:
+            cleaned_text += char
+    return cleaned_text
 
-punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
-cleaned_text = ""
-for char in text:
-    if char not in punctuation:
-        cleaned_text += char
 
-words = cleaned_text.split()
-word_count = len(words)
+def count_words(words):
+    return len(words)
 
-vowels = "аеёиоуыэюя"
-vowel_count = 0
-for char in cleaned_text:
-    if char in vowels:
-        vowel_count += 1
 
-longest_word = ""
-for word in words:
-    if len(word) > len(longest_word):
-        longest_word = word
+def find_longest_word(words):
+    longest_word = ""
+    for word in words:
+        if len(word) > len(longest_word):
+            longest_word = word
+    return longest_word
 
-word_frequency = {}
-for word in words:
-    if word in word_frequency:
-        word_frequency[word] += 1
-    else:
-        word_frequency[word] = 1
 
-print(f"1. Общее количество слов: {word_count}", f"2. Самое длинное слово: '{longest_word}'", f"3. Количество гласных букв: {vowel_count}", "4. Частота слов:", sep="\n")
-for word, freq in word_frequency.items():
-    print(f"   '{word}': {freq}")
+def count_vowels(text):
+    vowels = "аеёиоуыэюя"
+    vowel_count = 0
+    for char in text:
+        if char in vowels:
+            vowel_count += 1
+    return vowel_count
+
+
+def count_word_frequency(words):
+    word_frequency = {}
+    for word in words:
+        if word in word_frequency:
+            word_frequency[word] += 1
+        else:
+            word_frequency[word] = 1
+    return word_frequency
+
+
+def analyze_text(text):
+    cleaned_text = clean_text(text)
+    words = cleaned_text.split()
+    word_count = count_words(words)
+    longest_word = find_longest_word(words)
+    vowel_count = count_vowels(cleaned_text)
+    word_frequency = count_word_frequency(words)
+
+    return {
+        'word_count': word_count,
+        'longest_word': longest_word,
+        'vowel_count': vowel_count,
+        'word_frequency': word_frequency
+    }
+
+def print_results(results):
+    print(f"1. Общее количество слов: {results['word_count']}", f"2. Самое длинное слово: '{results['longest_word']}'", f"3. Количество гласных букв: {results['vowel_count']}", "4. Частота слов:", sep="\n")
+    for word, freq in results['word_frequency'].items():
+        print(f"   '{word}': {freq}")
+
+text = input("Введите текст: ")
+results = analyze_text(text)
+print_results(results)
+
 
 
 
