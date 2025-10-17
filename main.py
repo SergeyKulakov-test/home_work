2f297c8a796e0e602fd14dbd80443e18a3fdd714
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-17 17:01:31 +0300

    ДЗ: Сложение элементов списков_корректировка

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 list1 = [1, 2, 3, 4, 5, 6]
 list2 = [10, 20, 30, 40, 50]
 
-min_length = min(len(list1), len(list2))
+max_length = max(len(list1), len(list2))
 
 result_list = []
-for i in range(min_length):
-    sum_elements = list1[i] + list2[i]
-    result_list.append(sum_elements)
+for i in range(max_length):
+    element1 = list1[i] if i < len(list1) else 0
+    element2 = list2[i] if i < len(list2) else 0
+    result_list.append(element1 + element2)
 
 print("Список с суммами элементов:", result_list)
