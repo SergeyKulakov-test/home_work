02e67ad322a11ea333297728f7807442c25efc62
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-17 08:31:20 +0300

    ДЗ:Замена элементов списка

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-def sum_even_numbers():
-    sum = 0
-    for i in range(0, 100):
-        if i % 2 == 0:
-            sum += i
-    return print(f"Сумма четных чисел до 100: {sum}")
+my_list = []
 
-def square_odd_numbers():
-    square_odd = [1, 3, 5, 7, 9]
-    square_odd = [x**2 for x in square_odd]
-    return print(f"Список квадратов нечетных чисел до 10: {square_odd}")
+for i in range(5):
+    text = input(f"Введите строку {i+1}: ")
+    my_list.append(text)
 
-def count_positive_numbers():
-    count = 0
-    while True:
-        try:
-            number = float(input("Введите число: "))
-            if number < 0:
-                break
-            else:
-                count += 1
-        except ValueError:
-            print("Ошибка! Пожалуйста, введите число.")
-    return print(f"Количество введеных не отрицательных чисел: {count}")
+my_list[0], my_list[-1] = my_list[-1], my_list[0]
 
-sum_even_numbers()
-square_odd_numbers()
-count_positive_numbers()
-
-
-
-
-
-
+print("Список после замены:", my_list)
