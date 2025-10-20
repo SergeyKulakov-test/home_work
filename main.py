9efda9910aedb28e9637e16a81e7b5fb365cef70
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-20 13:53:36 +0300

    ДЗ: Сложение элементов списков_корректировка_2

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 def input_number_list(list_name):
-    numbers = input(f"Введите числа для {list_name} через пробел: ")
-    return [int(num) for num in numbers.split()]
+    while True:
+        try:
+            numbers = input(f"Введите числа для {list_name} через пробел: ")
+            return [int(num) for num in numbers.split()]
+        except ValueError:
+            print("Ошибка! Пожалуйста, вводите только целые числа.")
 
-def sum_numbers(list1, list2):
-    max_length = max(len(list1), len(list2))
+def sum_numbers(list_1, list_2):
+    max_length = max(len(list_1), len(list_2))
     result_list = []
     for i in range(max_length):
-        element1 = list1[i] if i < len(list1) else 0
-        element2 = list2[i] if i < len(list2) else 0
-        result_list.append(element1 + element2)
+        element_1 = list_1[i] if i < len(list_1) else 0
+        element_2 = list_2[i] if i < len(list_2) else 0
+        result_list.append(element_1 + element_2)
     return print("Список с суммами элементов:", result_list)
 
-list1 = input_number_list("первого списка")
-list2 = input_number_list("второго списка")
+list_1 = input_number_list("первого списка")
+list_2 = input_number_list("второго списка")
 
-
-sum_numbers(list1, list2)
+sum_numbers(list_1, list_2)
