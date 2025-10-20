5886c30ba453e36123fb91c12656df6b56cefb15
Sergey <sergey.a.kulackov@yandex.ru>
2025-10-20 13:59:29 +0300

    ДЗ: Сложение элементов списков_корректировка_2

--- main.py
+++ main.py
@@ -6,14 +6,18 @@ def input_number_list(list_name):
         except ValueError:
             print("Ошибка! Пожалуйста, вводите только целые числа.")
 
+
 def sum_numbers(list_1, list_2):
-    max_length = max(len(list_1), len(list_2))
+    min_length = min(len(list_1), len(list_2))
     result_list = []
-    for i in range(max_length):
-        element_1 = list_1[i] if i < len(list_1) else 0
-        element_2 = list_2[i] if i < len(list_2) else 0
-        result_list.append(element_1 + element_2)
-    return print("Список с суммами элементов:", result_list)
+    for i in range(min_length):
+        result_list.append(list_1[i] + list_2[i])
+    if len(list_1) > min_length:
+        result_list.extend(list_1[min_length:])
+    else:
+        result_list.extend(list_2[min_length:])
+    print("Список с суммами элементов:", result_list)
+
 
 list_1 = input_number_list("первого списка")
 list_2 = input_number_list("второго списка")
